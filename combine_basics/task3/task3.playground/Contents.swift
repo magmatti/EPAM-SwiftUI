1a1d55f246bc2b944821c1e66a05ab9db2889b73
Mateusz Wójtowicz <matiwojt@gmail.com>
2025-08-19 19:00:33 +0200

    finished task 3 in combine basics

--- combine_basics/task3/task3.playground/Contents.swift
+++ combine_basics/task3/task3.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+import Combine
 
-var greeting = "Hello, playground"
+let namePublisher = Just("john")
+let surnamePublisher = Just("doe")
+
+let transformedNamePublisher = namePublisher
+    .map { $0.uppercased() }
+
+let transformedSurnamePublisher = surnamePublisher
+    .map { $0.lowercased() }
+
+let fullNamePublisher = transformedNamePublisher
+    .zip(transformedSurnamePublisher) { name, surname in
+        return "\(name) \(surname)"
+    }
+
+var cancellables = Set<AnyCancellable>()
+
+fullNamePublisher
+    .sink { print($0) }
+    .store(in: &cancellables)
